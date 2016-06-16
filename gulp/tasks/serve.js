c3afe448247fb20c03cdec3b9ccab3030bd24e07
Lubos Kmetko <lubos@xhtmlized.com>
2016-06-16 16:21:59 +0200

    Minor tasks updates

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -2,14 +2,12 @@
 
 var serveTask = function (gulp, plugins, config) {
   gulp.task('serve', function() {
-    var reload = plugins.browserSync.reload;
-
     plugins.browserSync.init({
       server: './'
     });
 
-    gulp.watch('*.php').on('change', reload); // PHP files in WP projects
-    gulp.watch(config.paths.src + '/templates/**/*.twig').on('change', reload); // Twig templates in WP projects
+    gulp.watch('*.php').on('change', plugins.browserSync.reload); // PHP files in WP projects
+    gulp.watch(config.paths.src + '/templates/**/*.twig').on('change', plugins.browserSync.reload); // Twig templates in WP projects
     gulp.watch(config.paths.src + '/templates/**/*.{twig,html}', ['templates']); // Build templates in front-end project
     gulp.watch(config.paths.src + '/styles/**/*', ['styles']);
   });
