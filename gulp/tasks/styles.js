433c0e09d66373342eafb394561d0a3c5dbb7960
Lubos Kmetko <lubos@xhtmlized.com>
2016-06-17 17:06:27 +0200

    Fixes #3, fixes #10

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var stylesTask = function (gulp, plugins, config) {
+var stylesTask = function (gulp, plugins, config, helpers) {
   gulp.task('styles', function() {
     var src = config.paths.src + '/styles/*.scss';
     var dest = config.paths.dest + '/styles';
     
     return gulp.src(src)
+      .pipe(plugins.plumber(helpers.onError))
       .pipe(plugins.cssGlobbing({ extensions: ['.scss', '.css'] }))
       .pipe(plugins.sass())
       .pipe(gulp.dest(dest))
