f72b46fd86c22f915f1b63851d8929bfb216b4b0
Lubos Kmetko <lubos@xhtmlized.com>
2016-06-20 17:17:08 +0200

    Fixes #5

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var templatesTask = function (gulp, plugins, config) {
+var templatesTask = function (gulp, plugins, config, helpers) {
   gulp.task('templates', function() {
     var src = config.paths.src + '/templates/*.twig';
     var dest = config.paths.dest;
 
     return gulp.src(src)
+      .pipe(plugins.plumber(helpers.onError))
       .pipe(plugins.twigUpToDate({ errorLogToConsole: true }))
+      .pipe(plugins.prettify({ indent_size: 2, preserve_newlines: true }))
       .pipe(gulp.dest(dest))
       .on('end', plugins.browserSync.reload);
   });
