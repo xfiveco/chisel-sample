34140976babdaaa47bea12627eff27c3ae584bee
Lubos Kmetko <lubos@xhtmlized.com>
2016-06-21 16:59:59 +0200

    Fixes #8

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,16 @@ var stylesTask = function (gulp, plugins, config, helpers) {
   gulp.task('styles', function() {
     var src = config.paths.src + '/styles/*.scss';
     var dest = config.paths.dest + '/styles';
+
+    var postcssPlugins = [
+      require('autoprefixer')()
+    ];
     
     return gulp.src(src)
       .pipe(plugins.plumber(helpers.onError))
       .pipe(plugins.cssGlobbing({ extensions: ['.scss', '.css'] }))
       .pipe(plugins.sass())
+      .pipe(plugins.postcss(postcssPlugins))
       .pipe(gulp.dest(dest))
       .pipe(plugins.browserSync.stream());
   });
