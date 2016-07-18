e842751d076d1655763fb3ce4daefa889b2a63cb
Lubos Kmetko <lubos@xhtmlized.com>
2016-07-18 17:08:30 +0200

    Fixes #23

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -8,7 +8,6 @@ var serveTask = function (gulp, plugins, config) {
 
     gulp.watch(config.paths.src + '/styles/**/*', ['styles-watch']);
     gulp.watch('*.php').on('change', plugins.browserSync.reload); // PHP files in WP projects
-    gulp.watch(config.paths.src + '/templates/**/*.twig').on('change', plugins.browserSync.reload); // Twig templates in WP projects
     gulp.watch(config.paths.src + '/templates/**/*.{twig,html}', ['templates-watch']); // Build templates in front-end project
   });
 };
